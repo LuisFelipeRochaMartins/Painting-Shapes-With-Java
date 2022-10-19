b9bc9b2731dc1051aa258c88b92551973e6c5973
Luís Felipe Rocha Martins <luisfelipetochamartins>
2022-10-19 07:54:53 -0300

    adding Exercise

--- src/Cubo.java
+++ src/Cubo.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Cubo {
+import Objects.Cube;
+import java.util.Scanner;
+
+public class Cubo {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.print("Informe o lado do cubo: ");
+        double n = input.nextDouble();
+        Cube cubo = new Cube(n);
+
+
+        System.out.print("Informe o rendimento da tinta : ");
+        double x = input.nextDouble();
+
+        cubo.setRendimento(x);
+
+        System.out.print("Informe o tipo da tinta : ");
+        double y = input.nextDouble();
+
+        cubo.setTipo(y);
+
+        System.out.println(cubo);
+
+    }
 }
--- src/Objects/Cube.java
+++ src/Objects/Cube.java
@@ -1,2 +1,108 @@
-package Objects;public class Cube {
+package Objects;
+
+import jdk.jfr.Experimental;
+
+public class Cube {
+    public Cube(double lado) throws Exception {
+        setVolume(lado);
+        setLado(lado);
+    }
+    private double lado;
+
+    private double rendimento;
+
+    private double tipo;
+
+    private double volume;
+
+    private double latas;
+
+    private double preco;
+
+    private double litros;
+
+    private double areaTotal;
+
+    public void setLado(double lado)throws Exception {
+        if (lado>0) {
+            this.lado = lado;
+        }else{
+            throw new Exception("Não é possível ter lados negativos ou nulos!");
+        }
+    }
+
+    public double getRendimento() {
+        return rendimento;
+    }
+
+    public void setRendimento(double rendimento)throws Exception {
+        if (rendimento >0) {
+            this.rendimento = rendimento;
+            setLitros(rendimento);
+        }else{
+            throw new Exception("Não é possível ter rendimento negativo ou nulo!");
+        }
+    }
+
+    public double getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(double tipo){
+        if (tipo==1){
+            this.tipo = 101.90f;
+        }if (tipo==2){
+            this.tipo = 212.45f;
+        }if (tipo==3){
+            this.tipo = 345.56f;
+        }
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double lado) {
+        this.volume = lado*lado*lado;
+    }
+
+    public double getLatas() {
+        setPreco(latas);
+        return latas;
+
+    }
+
+    public void setLatas(double litros) {
+        this.latas = Math.ceil(litros/18);
+
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double latas) {
+        this.preco = latas*tipo;
+    }
+
+    public double getLitros() {
+        return litros;
+    }
+
+    public void setLitros(double rendimento) {
+        this.litros = lado*lado*6/rendimento;
+        setLatas(this.litros);
+    }
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("Área da Base ").append(lado*lado+"\n");
+        sb.append("Área Total : ").append(lado*lado*6+"\n");
+        sb.append("Volume : ").append(getVolume()+"\n");
+        sb.append("Diagonal do Cubo : ").append(lado*Math.sqrt(3)+"\n");
+        sb.append("Litros de Tinta : ").append(getLitros()+"\n");
+        sb.append("Latas de Tinta : ").append(getLatas()+"\n");
+        sb.append("Valor total : R$ ").append(getPreco());
+        return sb.toString();
+    }
 }
